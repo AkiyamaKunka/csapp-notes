e36ea80b0eeb28168f61f7a6ce85679812655699
Junjia Wang <wong13626@gmail.com>
2022-01-13 16:22:17 -0600

    Update attack_lab.c

--- attack_lab.c
+++ attack_lab.c
@@ -5,7 +5,7 @@ cat exploit.txt | ./hex2raw | ./ctarget -q
 // command to debug in gdb
 run exploit.txt > ./hex2raw -q
 // or
-run < exploit_raw.txt
+run < exploit_raw.txt -q
 
 
 // disassemble the binary file to asm
