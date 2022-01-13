e7e834377505b3f0650099e65b3c33cf532a3ef2
Junjia Wang <wong13626@gmail.com>
2022-01-13 11:56:28 -0600

    Update attack_lab.c

--- attack_lab.c
+++ attack_lab.c
@@ -1,9 +1,15 @@
 
 // command to run the program with customized hex code
 cat exploit.txt | ./hex2raw | ./ctarget -q
+
 // command to debug in gdb
 run exploit.txt > ./hex2raw -q
+// or
+run < exploit-raw.txt
+
+
 // disassemble the binary file to asm
 objdump -d ctarget > ctarget.d
+
 // convert txt hex to the format that the program can read
 bash> ./hex2raw < exploit.txt >| exploit_raw.txt
