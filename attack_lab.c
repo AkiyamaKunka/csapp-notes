f8d0fd61c8392ffe81debb68d7dce12e7a67d4ea
Junjia Wang <wong13626@gmail.com>
2022-01-13 14:13:37 -0600

    Update attack_lab.c

--- attack_lab.c
+++ attack_lab.c
@@ -23,4 +23,21 @@ AA AA AA AA AA AA AA AA
 AA AA AA AA AA AA AA AA
 c0 17 40 00 00 00 00 00
   
-//
+
+# Example of hand-generated assembly code
+pushq $0xabcdef # Push value onto stack
+addq $17,%rax # Add 17 to %rax
+movl %eax,%edx # Copy lower 32 bits of %eax to %edx
+
+// use command to compile and disassemble them to generate machine code
+unix> gcc -c example.s
+unix> objdump -d example.o > example.d
+
+
+// will get file
+example.o: file format elf64-x86-64
+Disassembly of section .text:
+0000000000000000 <.text>:
+0: 68 ef cd ab 00 pushq $0xabcdef
+5: 48 83 c0 11 add $0x11,%rax
+9: 89 c2 mov %eax,%edx
