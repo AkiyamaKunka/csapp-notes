59de50803ed4c8ffa575fcab5b0acccbf2c285db
Junjia Wang <wong13626@gmail.com>
2022-01-18 01:38:48 -0600

    Update cache_lab.c

--- cache_lab.c
+++ cache_lab.c
@@ -1,1 +1,25 @@
-// hello world!
+#include "cachelab.h"
+
+
+typedef struct {
+    int valid; // 1 is valid
+    int tag;
+    int * block;
+    int lru_stamp; // time stamp of LRU stratgy
+} Line;
+
+typedef struct {
+    int S;
+    int E;
+    int B;
+    Line *** lines;
+} Cache;
+
+
+
+int main()
+{   
+    
+    printSummary(0, 0, 0);
+    return 0;
+}
