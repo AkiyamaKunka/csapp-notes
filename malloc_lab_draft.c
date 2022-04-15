a9174636d65bb947d587278a81d9b1c98a4cec3d
Junjia Wang <wong13626@gmail.com>
2022-04-14 20:23:42 -0500

    Update malloc_lab_draft.c

--- malloc_lab_draft.c
+++ malloc_lab_draft.c
@@ -1,6 +1,22 @@
-#include "mem.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-extern BLOCK_HEADER *first_header;
+// **************************************
+// **********   BLOCK_HEADER   **********
+// **************************************
+// block_size = header_size + payload_size + padding 
+// block_size must be divisible by 16
+// alloc_bit = 1 if allocated and 0 if free
+// size_alloc = block_size + alloc_bit
+// payload = size requested for payload by the user of an allocated block or available size of a free block (used for performance analysis)
+// performance analysis is provided in Mem_Dump function
+typedef struct{
+    int size_alloc; 
+    int payload;
+} BLOCK_HEADER;
+
+BLOCK_HEADER *first_header;
 
 // return a pointer to the payload
 // if a large enough free block isn't available, return NULL
